92bcd5c8fc15e694835a827e5987bdd8893ed031
Daniel Micay <danielmicay@gmail.com>
2012-11-01 22:16:10 -0400

    add function to generate html pages from rst

--- wiki.py
+++ wiki.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import os.path as path
+
 import pygit2 as git
 from bottle import route, run, static_file
+from docutils.core import publish_file
 
 repo = git.init_repository("repo", False)
 
@@ -10,6 +13,11 @@ if 'refs/heads/master' not in repo.listall_references():
     tree = repo.TreeBuilder().write()
     repo.create_commit('refs/heads/master', author, author,
                        'initialize repository', tree, [])
+
+def generate_html_page(name):
+    publish_file(source_path=path.join("repo", name + ".rst"),
+                 destination_path=path.join("generated", name + ".html"),
+                 writer_name="html")
 
 @route('/page/<filename>.rst')
 def page(filename):
