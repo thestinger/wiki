db8b2040db6fc9e153d8638e8674a272c7025a23
Daniel Micay <danielmicay@gmail.com>
2012-11-01 22:14:15 -0400

    initialize git repository

--- wiki.py
+++ wiki.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+import pygit2 as git
 from bottle import run
+
+repo = git.init_repository("repo", False)
+
+if 'refs/heads/master' not in repo.listall_references():
+    author = git.Signature('wiki', 'danielmicay@gmail.com')
+    tree = repo.TreeBuilder().write()
+    repo.create_commit('refs/heads/master', author, author,
+                       'initialize repository', tree, [])
 
 run(host='localhost', port=8080)
