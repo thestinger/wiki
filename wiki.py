12930c2c79ca868810ad4479f59265abf4ed7ad9
Daniel Micay <danielmicay@gmail.com>
2012-11-02 14:18:27 -0400

    implement login tokens

--- wiki.py
+++ wiki.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import hmac
 import os.path as path
 from base64 import b64encode
+from hashlib import sha256
 from os import urandom
 
 import pygit2 as git
@@ -33,6 +35,23 @@ def generate_html_page(name):
                  destination_path=path.join("generated", name + ".html"),
                  writer_name="html")
 
+# TODO: this should be a persistent key, generated with something like openssl
+KEY = b64encode(urandom(256))
+
+def generate_mac(s):
+    return hmac.new(KEY, s.encode(), sha256).hexdigest()
+
+# TODO: these should also include a timestamp, and expire eventually
+
+def make_login_token(username):
+    return "|".join((generate_mac(username), username))
+
+def check_login_token(token):
+    "Return the username if the token is valid, otherwise None."
+    mac, username = token.split('|', 1)
+    if hmac.compare_digest(mac, generate_mac(username)):
+        return username
+
 @route('/page/<filename>.rst')
 def page(filename):
     return static_file(filename + '.rst', root="repo")
@@ -47,6 +66,9 @@ def log():
 
 @route('/update/json/<filename>', method='POST')
 def update(filename):
+    if check_login_token(request.json["token"]) is None:
+        return {"error": "invalid login token"}
+
     with open(path.join("repo", filename + '.rst'), "w") as f:
         f.write(request.json["page"])
     generate_html_page(filename)
@@ -71,5 +93,6 @@ def login():
     hashed, = connection.execute(sql.select([users.c.password_hash],
                                             users.c.username == username)).first()
     scrypt.decrypt(hashed, password, maxtime=0.5)
+    return {"token": make_login_token(username)}
 
 run(host='localhost', port=8080)
