f57cc07f39390e566e93fd011b7a543897d19edc
Daniel Micay <danielmicay@gmail.com>
2012-11-01 22:15:12 -0400

    add functions for viewing pages

--- wiki.py
+++ wiki.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import pygit2 as git
-from bottle import run
+from bottle import route, run, static_file
 
 repo = git.init_repository("repo", False)
 
@@ -10,5 +10,13 @@ if 'refs/heads/master' not in repo.listall_references():
     tree = repo.TreeBuilder().write()
     repo.create_commit('refs/heads/master', author, author,
                        'initialize repository', tree, [])
+
+@route('/page/<filename>.rst')
+def page(filename):
+    return static_file(filename + '.rst', root="repo")
+
+@route('/page/<filename>.html')
+def html_page(filename):
+    return static_file(filename + '.html', root="generated")
 
 run(host='localhost', port=8080)
